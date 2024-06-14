6361db553408a8ed2a4c6c3bf11bb1a1e6db5943
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-14 19:31:40 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,6 @@ function showSection(sectionId) {
     sections.forEach(section => {
         section.style.display = 'none';
     });
-    document.getElementById(sectionId).style.display = 'block';
-}
 
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
     document.getElementById(sectionId).style.display = 'block';
-}
-
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
 }
