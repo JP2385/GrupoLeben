d7e59075f01c820f9be82417c0f2a8fa7318ab59
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-15 14:31:43 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Función para abrir la primera hoja de cálculo
-    window.openSpreadsheet1 = function() {
-        const spreadsheetUrl1 = 'https://docs.google.com/spreadsheets/d/1IIAo4YHyBavQmA7iN3xVwhW9NncDwYU-3PpnL81d4TE/edit?usp=sharing';
-        window.open(spreadsheetUrl1, '_blank');
+    // Función para abrir una hoja de cálculo
+    window.openSpreadsheet = function(url) {
+        window.open(url, '_blank');
     }
 
-    // Función para abrir la segunda hoja de cálculo
-    window.openSpreadsheet2 = function() {
-        const spreadsheetUrl2 = 'https://docs.google.com/spreadsheets/d/1CBU7nXGDil2BPH5Ow2KOA7YgT9-LyGmtJSqBaOOmwng/edit?usp=drive_link';
-        window.open(spreadsheetUrl2, '_blank');
+    // Función para mostrar una sección
+    window.showSection = function(sectionId) {
+        const sections = document.querySelectorAll('.content-section');
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+        document.getElementById(sectionId).style.display = 'block';
     }
 });
-
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
-
-    document.getElementById(sectionId).style.display = 'block';
-}
-
