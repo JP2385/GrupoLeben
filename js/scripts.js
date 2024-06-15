fab54cb32502380f14225ff231d6e06a2ce865a4
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-14 21:39:39 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Función para abrir la hoja de cálculo
     window.openSpreadsheet = function() {
-        const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1VS3zsNmYvCeJTGgA1AlqovMaFurvjrZ_/edit?usp=sharing';
+        const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1VS3zsNmYvCeJTGgA1AlqovMaFurvjrZ_/edit?usp=drive_link&ouid=116849778907664010256&rtpof=true&sd=true';
         window.open(spreadsheetUrl, '_blank');
     }
 });
