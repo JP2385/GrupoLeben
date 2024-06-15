6c4f854594f0d26a7e8d4e031af49f3943f070fd
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-15 13:11:27 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Registrar el Service Worker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch(function(error) {
+            console.log('Service Worker registration failed:', error);
+        });
+    }
+
     // Función para abrir la primera hoja de cálculo
     window.openSpreadsheet1 = function() {
         const spreadsheetUrl1 = 'https://docs.google.com/spreadsheets/d/1IIAo4YHyBavQmA7iN3xVwhW9NncDwYU-3PpnL81d4TE/edit?usp=sharing';
