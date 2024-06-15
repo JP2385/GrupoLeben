700d33ae973671b1fc8a1bd6b7762d0858257c1d
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-14 21:29:48 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const SHEET_ID = '1VS3zsNmYvCeJTGgA1AlqovMaFurvjrZ_';
-    const SHEET_NAME = 'Vacaciones y Guardias 2024'; // Ajusta esto si tu hoja tiene un nombre diferente
-    const API_KEY = 'AIzaSyDvAqHTOqU0i151cbB3Amdyn1D91RlRf_c'; // Reemplaza con tu API Key
+    const SHEET_NAME = 'AIzaSyDvAqHTOqU0i151cbB3Amdyn1D91RlRf_c'; // Ajusta esto si tu hoja tiene un nombre diferente
+    const API_KEY = 'YOUR_API_KEY'; // Reemplaza con tu API Key
     const API_URL = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`;
 
     // Elemento del DOM para el contenido
