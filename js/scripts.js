983d21ab13bd84fc509a714a92cc11815cea86b8
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-14 22:04:25 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Función para abrir la hoja de cálculo
-    window.openSpreadsheet = function() {
-        const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1IIAo4YHyBavQmA7iN3xVwhW9NncDwYU-3PpnL81d4TE/edit?usp=sharing';
-        window.open(spreadsheetUrl, '_blank');
+    // Función para abrir la primera hoja de cálculo
+    window.openSpreadsheet1 = function() {
+        const spreadsheetUrl1 = 'https://docs.google.com/spreadsheets/d/1IIAo4YHyBavQmA7iN3xVwhW9NncDwYU-3PpnL81d4TE/edit?usp=drive_link'; 
+        window.open(spreadsheetUrl1, '_blank');
+    }
+
+    // Función para abrir la segunda hoja de cálculo
+    window.openSpreadsheet2 = function() {
+        const spreadsheetUrl2 = 'https://docs.google.com/spreadsheets/d/1CBU7nXGDil2BPH5Ow2KOA7YgT9-LyGmtJSqBaOOmwng/edit?usp=sharing';
+        window.open(spreadsheetUrl2, '_blank');
     }
 });
 
@@ -14,4 +20,3 @@ function showSection(sectionId) {
 
     document.getElementById(sectionId).style.display = 'block';
 }
-
