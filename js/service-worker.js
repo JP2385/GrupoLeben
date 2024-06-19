22044f3acafa882ce37364293760d88a5829088b
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-19 13:29:16 -0300

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -7,3 +7,30 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/js/scripts.js',
+                '/json/manifest.json',
+                '/images/icons/icon-192x192.png',
+                '/images/icons/icon-512x512.png'
+                // Add other resources you want to cache
+            ]).catch(function(error) {
+                console.error('Failed to cache', error);
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
