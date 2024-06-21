cbd18036add334db858eb21c28b8c72754e71197
JP2385 <165720201+JP2385@users.noreply.github.com>
2024-06-21 15:46:22 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    window.showSection = function(sectionId) {
-        const sections = document.querySelectorAll('.content-section');
-        sections.forEach(section => {
-            section.style.display = 'none';
-        });
-        document.getElementById(sectionId).style.display = 'block';
-    }
-});
