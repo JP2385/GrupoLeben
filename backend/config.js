6b8c0157642c6adedcad88b751979334b7f25763
Juan Pablo Serrano <jpserranogamarra@gmail.com>
2024-06-27 11:53:09 -0300

    Update backend config

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,8 @@
-module.exports = {
-    mongoUri: 'mongodb+srv://GrupoLeben:sliybILssZiPbQPK@grupolebencluster.l7f4nfa.mongodb.net/?retryWrites=true&w=majority&appName=GrupoLebenCluster',
-    jwtSecret: 'a8f33326051da7f9e0cc4cf8aedb4795dce7c23d1fdc1fce53c23f4acc7ab22967d987f5008d65dc20f1566d8d316d9f3344c5674f3024ada534725ec435d0e6'
-};
+require('dotenv').config();
 
-// sliybILssZiPbQPK
+module.exports = {
+    mongoUri: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    emailUser: process.env.EMAIL_USER,
+    emailPass: process.env.EMAIL_PASS
+};
