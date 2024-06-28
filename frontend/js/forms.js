0dd5242299c628f22fa24ee78e3b42d4a57fa7f1
Juan Pablo Serrano <jpserranogamarra@gmail.com>
2024-06-28 16:47:30 -0300

    Manejo de token

--- frontend/js/forms.js
+++ frontend/js/forms.js
@@ -48,6 +48,37 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    // Verificar el token en localStorage al cargar la página de index.html
+    if (window.location.pathname === '/index.html') {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            window.location.href = 'login.html';
+        } else {
+            fetch('https://grupoleben-92f01f246848.herokuapp.com/index.html', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error('Failed to authenticate token');
+                }
+            })
+            .then(html => {
+                document.documentElement.innerHTML = html;
+            })
+            .catch(error => {
+                alert(error.message);
+                localStorage.removeItem('token');
+                window.location.href = 'login.html';
+            });
+        }
+    }
+
     // Cargar datos de inicio de sesión si remember-me está activado
     const rememberMe = localStorage.getItem('rememberMe');
     if (rememberMe === 'true') {
